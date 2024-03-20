e7edd204c05a51c5103beb07a3e6160948170822
Andrew Au <andrewau@microsoft.com>
2024-03-20 10:04:39 -0700

    AutoTrace

--- scripts/lab-setup.ps1
+++ scripts/lab-setup.ps1
@@ -4,7 +4,23 @@
 #
 ################################################################################
 
+$trace = $env:andrew_trace
 $log = $env:andrew_log
+
+if ($trace -eq "1")
+{
+    if ($IsWindows -or $ENV:OS)
+    {
+        remove-item C:\Dev\runtime\CoreLab\* -include *.nettrace
+        remove-item C:\Dev\diagnostics\src\Tools\dotnet-trace\* -include *.nettrace
+    }
+    else
+    {
+        remove-item "$($home)/git/runtime/CoreLab/*" -include *.nettrace
+        remove-item "$($home)/git/diagnostics/src/Tools/dotnet-trace/*" -include *.nettrace
+    }
+}
+
 if ($log -eq "1")
 {
     remove-item "$($logPath)*" -include CoreLab.*.log
